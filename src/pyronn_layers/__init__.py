70444bbe88301bc472780b78ff368a53808e9c1b
Stephan Seitz <stephan.seitz@fau.de>
2019-09-12 19:30:47 +0200

    Make PYRO-NN-Layers compile

--- src/pyronn_layers/__init__.py
+++ src/pyronn_layers/__init__.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
+import glob
+import os.path
+
+from pkg_resources import DistributionNotFound, get_distribution
+
+import pyronn_layers
+import pystencils_autodiff.tensorflow_jit
 
 try:
     # Change here if project is renamed and does not equal the package name
@@ -9,3 +15,15 @@ except DistributionNotFound:
     __version__ = 'unknown'
 finally:
     del get_distribution, DistributionNotFound
+
+
+_pyronn_layers_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'PYRO-NN-Layers')
+_pyronn_layers_sources = glob.glob(os.path.join(_pyronn_layers_dir, '*.cc'))
+print(',\n'.join(_pyronn_layers_sources))
+_pyronn_layers_module = pystencils_autodiff.tensorflow_jit.compile_sources_and_load(
+    [],
+    _pyronn_layers_sources,
+    additional_compile_flags=['-I' + _pyronn_layers_dir, '-DGOOGLE_CUDA'])
+
+for obj in dir(_pyronn_layers_module):
+    setattr(pyronn_layers, obj, getattr(_pyronn_layers_module, obj))
---
+++ tests/test_import.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2019 Stephan Seitz <stephan.seitz@fau.de>
+#
+# Distributed under terms of the GPLv3 license.
+
+"""
+
+"""
+
+
+def test_import():
+    import pyronn_layers
