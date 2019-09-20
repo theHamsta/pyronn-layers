6338e2b4bc23c435be7eef9340ba3540a25c1cfb
Stephan Seitz <stephan.seitz@fau.de>
2019-09-20 11:44:55 +0200

    Parametrize test_pyronn_examples

--- tests/test_pyronn_examples.py
+++ tests/test_pyronn_examples.py
@@ -12,16 +12,18 @@ import sys
 from glob import glob
 from os.path import basename, dirname, join
 
+import pytest
 
-def test_pyronn_examples():
-    example_files_glob = join(dirname(__file__), 'PYRO-NN', 'examples', '**', '*.py')
-    example_files = glob(example_files_glob, recursive=True)
+example_files_glob = join(dirname(__file__), 'PYRO-NN', 'examples', '**', '*.py')
+example_files = glob(example_files_glob, recursive=True)
 
-    sys.path.append(join(dirname(__file__), 'PYRO-NN'))
+sys.path.append(join(dirname(__file__), 'PYRO-NN'))
+
+
+@pytest.mark.parametrize('file', example_files)
+def test_pyronn_examples(file):
 
     for file in example_files:
-        print(file)
-        print(basename(file))
         spec = importlib.util.spec_from_file_location(basename(file), file)
         foo = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(foo)
